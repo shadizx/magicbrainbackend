fdfb00ed6ad52fcf266d3b31161311fc4a6c2a65
Shadi Zoldjalali <shadizold@gmail.com>
2023-01-10 11:05:35 -0800

    Fix consolog log for database url

--- server.js
+++ server.js
@@ -104,5 +104,5 @@ app.put('/image', (req, res) => {
 })
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('app is running on port ${process.env.port}');
+    console.log(`app is running on port ${process.env.PORT}`);
 })
