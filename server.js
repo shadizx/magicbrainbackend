bafde6d2bdd24ab173bdf8729a3d03098f73809e
Shadi Zoldjalali <shadizold@gmail.com>
2023-03-10 12:10:07 -0800

    Update code for render deployment

--- server.js
+++ server.js
@@ -1,58 +1,121 @@
-const express = require('express');
-const bp = require('body-parser');
-// const db = require('knex')({
-//     client: 'pg',
-//     connection: {
-//         connectionString: process.env.DATABASE_URL,
-//         ssl: {
-//             rejectUnauthorized: false
-//         }
-//     }
-// });
-const bcrypt = require('bcrypt');
+const express = require("express");
+const bp = require("body-parser");
+const db = require("knex")({
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+});
+const bcrypt = require("bcrypt");
 const cors = require("cors");
 const saltRounds = 10;
 
 const app = express();
 const corsOptions = {
-    origin: '*',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-}
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 app.use(bp.json());
 
-app.get('/', (req, res) => { res.send('Backend is running successfully!') });
-app.post('/signin', (req, res) => {
-    res.json("Found");
-})
-
-app.post('/signup', (req, res) => {
-    res.json("Found");
-})
-
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({ id }).then(user => {
-        if (user.length) {
-            res.json(user);
-        }
-        else {
-            res.status(400).json('Not found');
-        }
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully!");
+});
+app.post("/signin", (req, res) => {
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then((data) => {
+      const validCred = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (validCred) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => res.status(400).json("Unable to get user"));
+      } else {
+        res.status(400).json("Wrong credentials");
+      }
     })
-        .catch(err => res.status(400).json('Error getting user'));
-})
+    .catch((err) => res.status(400).json("Wrong credentials"));
+});
 
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id).increment('entries', 1).returning('entries')
-        .then(entries => {
-            res.json(entries[0]);
+app.post("/signup", (req, res) => {
+  const { email, name, password } = req.body;
+  var validCheck = true;
+
+  // check if there already exists this email in db
+  db.select("email")
+    .from("login")
+    .where("email", "=", email)
+    .then((data) => {
+      if (data.length == 1) {
+        validCheck = false;
+        res.status(400).json("User already exists");
+      }
+    });
+
+  const hash = bcrypt.hashSync(password, saltRounds);
+  if (validCheck == true) {
+    db.transaction((trx) => {
+      trx("login")
+        .insert({
+          hash: hash,
+          email: email,
         })
-        .catch(err => res.status(400).json('Unable to get entries'));
-})
+        .returning("email")
+        .then((loginEmail) => {
+          return trx("users")
+            .returning("*")
+            .insert({
+              email: loginEmail[0].email,
+              name: name,
+              joined: new Date(),
+            })
+            .then((user) => {
+              res.json(user);
+            });
+        })
+        .then(trx.commit)
+        .catch(trx.rollback);
+    }).catch((err) => console.log(err));
+  }
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  db.select("*")
+    .from("users")
+    .where({ id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user);
+      } else {
+        res.status(400).json("Not found");
+      }
+    })
+    .catch((err) => res.status(400).json("Error getting user"));
+});
+
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json("Unable to get entries"));
+});
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on port ${process.env.PORT}`);
-})
+  console.log(`app is running on port ${process.env.PORT}`);
+});
